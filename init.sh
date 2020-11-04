7dc1fdcb0245fbbd10d5a498b013adfe536b696b
yury <yury@videoexpertsgroup.com>
2020-11-04 18:23:41 +0700

    Proxying requests to gunicorn app

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
+# start nginx server
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+# run gunicorn application server
+gunicorn -b 0.0.0.0:8080 hello:app
