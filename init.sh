2efc0eb1ae54f59b5d1a797dd7899648f10d0a30
Yury Altukhov <yury@videoexpertsgroup.com>
2020-12-22 23:28:09 +0700

    change init

--- init.sh
+++ init.sh
@@ -3,7 +3,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 # run gunicorn application server
-gunicorn -b 0.0.0.0:8080 hello:app &
-
 cd ask
 gunicorn -b 0.0.0.0:8000 ask.wsgi &
